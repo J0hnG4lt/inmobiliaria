52b3fb32252282802b6aba09d03b8a525c743a56
al3x4nd3r <alexanderstower@gmail.com>
2017-04-07 18:40:16 +0000

    Ya corre angular2

--- inmobiliaria/urls.py
+++ inmobiliaria/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
