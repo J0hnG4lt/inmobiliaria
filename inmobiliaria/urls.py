bb99c782831329854654193cc6377f75810c422a
Georvic Tur <alexanderstower@gmail.com>
2017-04-16 11:25:37 -0400

    Agregada versión inicial del controlador de vinmuebles

--- inmobiliaria/urls.py
+++ inmobiliaria/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^vinmuebles/(?P<pagina>\d+)', views.vInmuebles),
     url(r'^$', views.index, name="index")
 ]
 
--- inmobiliaria/views.py
+++ inmobiliaria/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
+from django.core import serializers
+from vinmuebles.models import OfertaDeInmueble
 
 def index(request):
     template = loader.get_template('index.html')
@@ -10,3 +12,30 @@ def index(request):
             'latest_question_list': []
         }
     return HttpResponse(template.render(context, request))
+
+def vInmuebles(request, pagina=None) :
+	
+	if pagina : 
+		pagina = int(pagina)
+	else :
+		pagina = 0
+
+	numeroDeInmueblesPorPagina=10
+	cotaInferior = pagina*numeroDeInmueblesPorPagina
+	cotaSuperior = (pagina+1)*numeroDeInmueblesPorPagina
+
+	todasLasOfertas = OfertaDeInmueble.objects.all()
+	numeroTotalDeOfertas = len(todasLasOfertas)
+
+	if cotaSuperior > numeroTotalDeOfertas :
+		cotaSuperior = numeroTotalDeOfertas
+		cotaInferior = numeroTotalDeOfertas - numeroDeInmueblesPorPagina
+
+		if cotaInferior < 0 :
+			cotaInferior = 0
+	
+	ofertas = todasLasOfertas[cotaInferior:cotaSuperior]
+
+	datos = serializers.serialize('json', ofertas)
+	
+	return HttpResponse(datos, content_type='application/json')
