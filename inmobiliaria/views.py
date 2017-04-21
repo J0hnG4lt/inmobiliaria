10421f327d6f0e8ee148f6df091550427e0fa04b
Georvic Tur <alexanderstower@gmail.com>
2017-04-21 02:34:54 -0400

    Arreglado el controlador para que muestre los nombres de los campos foráneaos en vInmuebles. Ver issue #8

--- inmobiliaria/views.py
+++ inmobiliaria/views.py
@@ -4,7 +4,8 @@ from django.template import Context, loader
 from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
 from django.core import serializers
-from vinmuebles.models import OfertaDeInmueble
+from vinmuebles.models import OfertaDeInmueble, TipoDeOperacion, TipoDeInmueble
+import json
 
 def index(request):
     template = loader.get_template('index.html')
@@ -12,6 +13,8 @@ def index(request):
             'latest_question_list': []
         }
     return HttpResponse(template.render(context, request))
+
+
 
 def vInmuebles(request, pagina=None) :
 	
@@ -37,5 +40,16 @@ def vInmuebles(request, pagina=None) :
 	ofertas = todasLasOfertas[cotaInferior:cotaSuperior]
 
 	datos = serializers.serialize('json', ofertas)
+	datos = json.loads(datos)
+	for oferta in datos :
+		tipoDeInmuebleID = oferta["fields"]["tipoDeInmueble"]
+		tipoDeInmueble = TipoDeInmueble.objects.filter(id=tipoDeInmuebleID)[0]
+		oferta["fields"]["tipoDeInmueble"] = tipoDeInmueble.nombreTipoDeInmueble
+
+		tipoDeOperacionID = oferta["fields"]["tipoDeOperacion"]
+		tipoDeOperacion = TipoDeOperacion.objects.filter(id=tipoDeOperacionID)[0]
+		oferta["fields"]["tipoDeOperacion"] = tipoDeOperacion.nombreDelTipoDeOperacion
 	
+	datos = json.dumps(datos)
+
 	return HttpResponse(datos, content_type='application/json')
